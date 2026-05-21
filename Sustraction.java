6548fdf2cae8cddaf7e59b03d1bfb0c4509c18ad
Mayank7838838481 <saxenmayank3883@gmail.com>
2026-05-21 18:06:05 +0530

    first commit

---
+++ Sustraction.java
@@ -0,0 +1,8 @@
+public class Sustraction {
+    static void main(String[] args) {
+        int a=10, b=4;
+        System.out.println("enter the values");
+        int sum =a-b;
+        System.out.println(sum);
+    }
+}
