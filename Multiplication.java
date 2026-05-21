2455339957270a384c8d8159f4c785cb341be7a9
Mayank7838838481 <saxenmayank3883@gmail.com>
2026-05-21 18:11:27 +0530

    first commit

---
+++ Multiplication.java
@@ -0,0 +1,8 @@
+public class Multiplication {
+    static void main(String[] args) {
+        int a=56,b=54;
+        System.out.println("Enter the values");
+        int mul=a*b;
+        System.out.println(mul);
+    }
+}
